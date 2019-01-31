bf6567d7a5ed5154d07ec365e5d6b20ff4d7a09c
Roman <nosferat@bk.ru>
2019-01-31 12:12:37 +0300

    Create run.bat

---
+++ sql/run.bat
@@ -0,0 +1,6 @@
+mysql -u root -p magic < language.sql --verbose 
+pause
+mysql -u root -p magic < format_and_legality.sql --verbose 
+pause
+mysql -u root -p magic < card_types.sql --verbose 
+pause
